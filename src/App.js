cd0ae0559e09d4ccf5874140237b193521f38742
Osama Bin Azam <osamabinazam@gmail.com>
2023-05-06 12:25:03 +0500

    Define Winner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import './App.css';
 import {useState} from 'react'    //Hook
 
 
+function calculateWinner (squares){
+
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (let i=0; i< lines.length; i++){
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 // Squares on the board
 function Square({value , onSquareClick}){
@@ -20,13 +42,24 @@ function Square({value , onSquareClick}){
 // Playboard
 export function Board(){
 
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick (i){
 
+    // Do not change the state of Square
+    if(squares[i] || calculateWinner(squares))
+      return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext){
+      nextSquares[i] ='X';
+    }
+    else {
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext)
   }
   return (
     <>
